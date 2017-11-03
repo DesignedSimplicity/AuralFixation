0a5738fac9ffb8c69d7652578f8200ab38bd3f2b
Kevin <Kevin@X>
2017-11-03 21:42:35 +1300

    Starting XMAL dev

---
+++ AuralFixation.Api/AuralFixation.App/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using AuralFixation.Api;
+
+namespace AuralFixation.App
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Service _service;
+		private PlayerTree _model;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			BuildViewModel();
+		}
+
+		public void BuildViewModel()
+		{
+			_service = new Service();
+
+			_model = new PlayerTree();
+
+			foreach (var reader in _service.ListReaders())
+			{
+				reader.Init();
+
+				var r = new ReaderNode(reader);
+				_model.Readers.Add(r);
+
+				foreach (var category in reader.Categories)
+				{
+					var uri = System.IO.Path.Combine(@"H:\Music\Albums\__ICONS", category + ".jpg");
+					var i = new BitmapImage(new Uri(uri));
+					var c = new CategoryNode(category, i);
+					r.Categories.Add(c);
+				}
+			}
+
+			listView.ItemsSource = _model.Readers[0].Categories;
+			items.ItemsSource = _model.Readers[0].Categories;
+		}
+	}
+}
---
+++ AuralFixation.Api/AuralFixation.App/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AuralFixation.Api.Model;
+using System.Windows.Media.Imaging;
+
+namespace AuralFixation.App
+{
+	public class PlayerTree
+	{
+		public List<ReaderNode> Readers { get; set; } = new List<ReaderNode>();
+	}
+
+
+	public class ReaderNode
+	{
+		public ReaderNode(IReader reader) { Reader = reader; }
+
+		public IReader Reader { get; private set; } 
+
+		public List<CategoryNode> Categories { get; set; } = new List<CategoryNode>();
+	}
+
+	public class CategoryNode
+	{
+		public CategoryNode(string category, BitmapImage icon)
+		{
+			Name = category;
+			Icon = icon;
+		}
+
+		public string Name { get; private set; }
+
+		public BitmapImage Icon { get; private set; }
+	}
+}
